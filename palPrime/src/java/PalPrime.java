71ca7f3b6b70cfcb514c8a8523cda3e85e2391e3
21cse095 <111353888+21cse095@users.noreply.github.com>
2023-12-28 13:56:01 +0530

    Add files via upload

---
+++ palPrime/src/java/PalPrime.java
@@ -0,0 +1,37 @@
+
+ class PalPrime {
+    public PalPrime(int number, String message) {
+        if (isPalPrime(number)) {
+            System.out.println("Number " + number + " is " + message);
+        }
+    }
+
+    private boolean isPalPrime(int number) {
+        return isPalindrome(number) && isPrime(number);
+    }
+
+    private boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        return originalNumber == reversedNumber;
+    }
+
+    private boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+   
+
